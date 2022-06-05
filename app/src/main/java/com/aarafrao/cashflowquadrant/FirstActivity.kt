47ef73195e8b59dceae278efeb96a73ef0540fca
Rao Aaraf Younus <aarafrao22@gmail.com>
2022-06-05 23:14:16 +0500

    v1

--- app/src/main/java/com/aarafrao/cashflowquadrant/FirstActivity.kt
+++ app/src/main/java/com/aarafrao/cashflowquadrant/FirstActivity.kt
@@ -1,16 +1,21 @@
 package com.aarafrao.cashflowquadrant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aarafrao.cashflowquadrant.databinding.ActivityFirstBinding
 
 class FirstActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityFirstBinding
-            override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var binding: ActivityFirstBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-                binding
-        setContentView(R.layout.activity_first)
+        binding = ActivityFirstBinding.inflate (layoutInflater)
+        setContentView(binding.root)
 
+        binding.layout1.setOnClickListener {
+            val intent: Intent = Intent(this,PdfActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
--- app/src/main/java/com/aarafrao/cashflowquadrant/PdfActivity.kt
+++ app/src/main/java/com/aarafrao/cashflowquadrant/PdfActivity.kt
@@ -1,38 +1,63 @@
 package com.aarafrao.cashflowquadrant
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.aarafrao.cashflowquadrant.databinding.ActivityFirstBinding
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.edit
 import com.aarafrao.cashflowquadrant.databinding.ActivityPdfBinding
 import com.github.barteksc.pdfviewer.PDFView
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener
+import com.github.barteksc.pdfviewer.util.FitPolicy
 
 
-class PdfActivity : AppCompatActivity() {
+class PdfActivity : AppCompatActivity(), OnPageChangeListener,View.OnClickListener {
 
     private lateinit var binding: ActivityPdfBinding
-    private var pagesNo: Int = 1
+    private lateinit var sharedPref: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPdfBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.pdfView.fromAsset("cashflow1.pdf").pages(pagesNo).load()
 
+        setSupportActionBar(binding.toolbar)
+        sharedPref = getSharedPreferences("myPref", MODE_PRIVATE)
+        var pagesNo = sharedPref.getInt("page", 1)
+        var nightMode:Boolean= false
 
-
+//        if ()
+        binding.pdfView.fromAsset("cashflow1.pdf")
+            .defaultPage(pagesNo)
+            .onPageChange(this)
+//            .nightMode()
+            .pageFitPolicy(FitPolicy.BOTH)
+            .nightMode(nightMode)
+            .load()
 
 
     }
 
-    override fun onStop() {
-        super.onStop()
-        pagesNo = binding.pdfView.currentPage
+
+    override fun onPageChanged(page: Int, pageCount: Int) {
+        editor = sharedPref.edit()
+        editor.putInt("page", page)
+        editor.apply()
     }
 
-    override fun onPause() {
-        super.onPause()
-        pagesNo = binding.pdfView.currentPage
+    override fun onClick(p0: View?) {
+
+        when(p0?.id){
+            R.id.nightMode ->    onNightModeChanged(MODE_NIGHT_YES)
+        }
+
+
     }
 
 }
